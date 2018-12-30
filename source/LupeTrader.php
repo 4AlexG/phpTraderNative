accd2ac52458df56686a83b3dd4d19a0723eb5f5
Joshua <joshua@lupecode.com>
2018-12-30 04:21:26 -0800

    Added the RSI class and use it in LupeTrader.
    Seriously, this class is smaller than the TA-lib trader_rsi function.

--- source/LupeTrader.php
+++ source/LupeTrader.php
@@ -2,6 +2,7 @@
 
 namespace LupeCode\phpTraderNative;
 
+use LupeCode\phpTraderNative\LupeTrader\MomentumIndicators\RSI;
 use LupeCode\phpTraderNative\TALib\Enum\MovingAverageType;
 use LupeCode\phpTraderNative\TALib\Enum\ReturnCode;
 
@@ -9,35 +10,47 @@ class LupeTrader extends Trader
 {
 
     /**
+     * Relative Strength Index
+     *
+     * @param array $real       Array of real values.
+     * @param int   $timePeriod [OPTIONAL] [DEFAULT 14, SUGGESTED 4-200] Number of period. Valid range from 2 to 100000.
+     *
+     * @return array Returns an array with calculated data.
+     * @throws \Exception
+     */
+    public static function rsi(array $real, int $timePeriod = 14): array
+    {
+        return RSI::rsi($real, $timePeriod);
+    }
+
+    /**
      * Slow Stochastic Relative Strength Index
      *
-     * @param array $real Array of real values.
-     * @param int   $rsi_period [OPTIONAL] [DEFAULT 14, SUGGESTED 4-200] Number of period. Valid range from 2 to 100000.
-     * @param int $fastK_Period [OPTIONAL] [DEFAULT 5, SUGGESTED 1-200] Time period for building the Fast-K line. Valid range from 1 to 100000.
-     * @param int $slowK_Period [OPTIONAL] [DEFAULT 3, SUGGESTED 1-200] Smoothing for making the Slow-K line. Valid range from 1 to 100000, usually set to 3.
-     * @param int $slowK_MAType [OPTIONAL] [DEFAULT TRADER_MA_TYPE_SMA] Type of Moving Average for Slow-K. MovingAverageType::* series of constants should be used.
-     * @param int $slowD_Period [OPTIONAL] [DEFAULT 3, SUGGESTED 1-200] Smoothing for making the Slow-D line. Valid range from 1 to 100000.
-     * @param int $slowD_MAType [OPTIONAL] [DEFAULT TRADER_MA_TYPE_SMA] Type of Moving Average for Slow-D. MovingAverageType::* series of constants should be used.
+     * @param array $real        Array of real values.
+     * @param int   $rsiPeriod   [OPTIONAL] [DEFAULT 14, SUGGESTED 4-200] Number of period. Valid range from 2 to 100000.
+     * @param int   $fastKPeriod [OPTIONAL] [DEFAULT 5, SUGGESTED 1-200] Time period for building the Fast-K line. Valid range from 1 to 100000.
+     * @param int   $slowKPeriod [OPTIONAL] [DEFAULT 3, SUGGESTED 1-200] Smoothing for making the Slow-K line. Valid range from 1 to 100000, usually set to 3.
+     * @param int   $slowKMAType [OPTIONAL] [DEFAULT TRADER_MA_TYPE_SMA] Type of Moving Average for Slow-K. MovingAverageType::* series of constants should be used.
+     * @param int   $slowDPeriod [OPTIONAL] [DEFAULT 3, SUGGESTED 1-200] Smoothing for making the Slow-D line. Valid range from 1 to 100000.
+     * @param int   $slowDMAType [OPTIONAL] [DEFAULT TRADER_MA_TYPE_SMA] Type of Moving Average for Slow-D. MovingAverageType::* series of constants should be used.
      *
      * @return array Returns an array with calculated data. [SlowK => [...], SlowD => [...]]
      * @throws \Exception
      */
-    public static function slowstochrsi(array $real, int $rsi_period = 14, int $fastK_Period = 5, int $slowK_Period = 3, int $slowK_MAType = MovingAverageType::SMA, int $slowD_Period = 3, int $slowD_MAType = MovingAverageType::SMA): array
+    public static function slowstochrsi(array $real, int $rsiPeriod = 14, int $fastKPeriod = 5, int $slowKPeriod = 3, int $slowKMAType = MovingAverageType::SMA, int $slowDPeriod = 3, int $slowDMAType = MovingAverageType::SMA): array
     {
-        $real   = \array_values($real);
-        $endIdx = count($real) - 1;
-        $rsi    = [];
-        self::checkForError(self::getMomentumIndicators()::rsi(0, $endIdx, $real, $rsi_period, self::$outBegIdx, self::$outNBElement, $rsi));
+        $real     = \array_values($real);
+        $endIdx   = count($real) - 1;
+        $rsi      = RSI::rsi($real, $rsiPeriod);
         $rsi      = array_values($rsi);
         $endIdx   = self::verifyArrayCounts([&$rsi]);
         $outSlowK = [];
         $outSlowD = [];
-        self::checkForError(self::getMomentumIndicators()::stoch(0, $endIdx, $rsi, $rsi, $rsi, $fastK_Period, $slowK_Period, $slowK_MAType, $slowD_Period, $slowD_MAType, self::$outBegIdx, self::$outNBElement, $outSlowK, $outSlowD));
+        self::checkForError(self::getMomentumIndicators()::stoch(0, $endIdx, $rsi, $rsi, $rsi, $fastKPeriod, $slowKPeriod, $slowKMAType, $slowDPeriod, $slowDMAType, self::$outBegIdx, self::$outNBElement, $outSlowK, $outSlowD));
 
         return [
             'SlowK' => self::adjustIndexes($outSlowK, self::$outBegIdx),
             'SlowD' => self::adjustIndexes($outSlowD, self::$outBegIdx),
         ];
     }
-
 }
---
+++ source/LupeTrader/MomentumIndicators/RSI.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace LupeCode\phpTraderNative\LupeTrader\MomentumIndicators;
+
+use LupeCode\phpTraderNative\TALib\Enum\ReturnCode;
+
+class RSI
+{
+
+    const RSI_DEFAULT_PERIOD = 14;
+
+    /** @var array */
+    protected $inputArray;
+    /** @var int */
+    protected $inputTimePeriod = self::RSI_DEFAULT_PERIOD;
+    /** @var array */
+    protected $outputArray;
+
+    /**
+     * @param array $inputArray
+     *
+     * @return $this
+     */
+    public function setInputArray(array $inputArray)
+    {
+        $this->inputArray = array_values($inputArray);
+
+        return $this;
+    }
+
+    /**
+     * @param int $inputTimePeriod
+     *
+     * @return $this
+     */
+    public function setInputTimePeriod(int $inputTimePeriod)
+    {
+        $this->inputTimePeriod = $inputTimePeriod;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOutputArray(): array
+    {
+        return $this->outputArray;
+    }
+
+    /**
+     * @param float $gains
+     * @param float $losses
+     *
+     * @return float
+     */
+    protected function rsiFormula(float $gains, float $losses): float
+    {
+        $rs  = ($gains / $this->inputTimePeriod) / ($losses / $this->inputTimePeriod);
+        $rsi = (100 - (100 / (1 + $rs)));
+
+        return $rsi;
+    }
+
+    /**
+     * @return $this
+     * @throws \Exception
+     */
+    public function calculate()
+    {
+        if (empty($this->inputArray) || empty($this->inputTimePeriod)) {
+            throw new \Exception("Input parameters missing", 7);
+        }
+        $count = count($this->inputArray);
+        if ($count < $this->inputTimePeriod) {
+            $this->outputArray = [];
+
+            return $this;
+        }
+        $gains  = 0;
+        $losses = 0;
+        for ($iterator = 1; $iterator <= $this->inputTimePeriod; $iterator++) {
+            $delta = $this->inputArray[$iterator] - $this->inputArray[$iterator - 1];
+            if ($delta > 0) {
+                $gains += $delta;
+            } else {
+                $losses += -$delta;
+            }
+        }
+        $rsi = [$this->inputTimePeriod => $this->rsiFormula($gains, $losses)];
+        for (; $iterator < $count; $iterator++) {
+            $delta = $this->inputArray[$iterator] - $this->inputArray[$iterator - 1];
+            if ($delta > 0) {
+                $gains  = $gains * ($this->inputTimePeriod - 1) / $this->inputTimePeriod + $delta;
+                $losses = $losses * ($this->inputTimePeriod - 1) / $this->inputTimePeriod + 0.;
+            } else {
+                $gains  = $gains * ($this->inputTimePeriod - 1) / $this->inputTimePeriod + 0.;
+                $losses = $losses * ($this->inputTimePeriod - 1) / $this->inputTimePeriod + -$delta;
+            }
+            $rsi[] = $this->rsiFormula($gains, $losses);
+        }
+        $this->outputArray = $rsi;
+
+        return $this;
+    }
+
+    /**
+     * Relative Strength Index
+     *
+     * @param array $inputArray      Array of real values.
+     * @param int   $inputTimePeriod [OPTIONAL] [DEFAULT RSI_DEFAULT_PERIOD, SUGGESTED 4-200] Number of period. Valid range from 2 to 100000.
+     *
+     * @return array Returns an array with calculated data.
+     * @throws \Exception
+     */
+    public static function rsi(array $inputArray, int $inputTimePeriod = self::RSI_DEFAULT_PERIOD): array
+    {
+        $self = new self();
+        $self
+            ->setInputTimePeriod($inputTimePeriod)
+            ->setInputArray($inputArray)
+            ->calculate()
+        ;
+
+        return $self->getOutputArray();
+    }
+
+}
