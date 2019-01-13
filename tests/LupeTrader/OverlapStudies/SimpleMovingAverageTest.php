5fb22b8c5d6f36f860707146b51e3b12aada447b
Joshua <joshua@lupecode.com>
2019-01-13 00:29:14 -0800

    Added the SMA test

---
+++ tests/LupeTrader/OverlapStudies/SimpleMovingAverageTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace LupeCode\phpTraderNativeTest\LupeTrader\OverlapStudies;
+
+use LupeCode\phpTraderNative\LupeTrader\Core\Exception;
+use LupeCode\phpTraderNative\LupeTrader\OverlapStudies\SimpleMovingAverage;
+use LupeCode\phpTraderNativeTest\TestingTrait;
+use PHPUnit\Framework\TestCase;
+
+class SimpleMovingAverageTest extends TestCase
+{
+    use TestingTrait;
+
+    /**
+     * @throws Exception
+     */
+    public function testCalculate()
+    {
+        $optInTimePeriod = 10;
+        $traderSMA = \trader_sma($this->High, $optInTimePeriod);
+        $lupeSMA = (new SimpleMovingAverage())->setInputArray($this->High)->setPeriod($optInTimePeriod)->calculate()->getOutputArray();
+        $this->assertEquals($traderSMA, $lupeSMA, '', 0.01);
+    }
+
+    /**
+     * @throws Exception
+     * @group exceptions
+     */
+    public function testCalculateException()
+    {
+        $simpleMovingAverageIndex = new SimpleMovingAverage();
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(Exception::INPUT_PARAMETERS_MISSING_CODE);
+        $this->expectExceptionMessage(Exception::INPUT_PARAMETERS_MISSING_MESSAGE);
+        $simpleMovingAverageIndex->calculate();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testCalculateTooShort()
+    {
+        $simpleMovingAverageIndex = new SimpleMovingAverage();
+        $expected              = [];
+        $simpleMovingAverageIndex
+            ->setPeriod(14)
+            ->setInputArray([1])
+            ->calculate()
+        ;
+        $this->assertEquals($expected, $simpleMovingAverageIndex->getOutputArray());
+    }
+
+}
